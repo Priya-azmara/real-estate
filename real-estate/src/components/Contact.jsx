b4b47934f9a926708bcc6015bc4ae580703cb0b6
Priya-azmara <priyadharsh2894@gmail.com>
2026-03-25 19:09:08 +0530

    real estate project completed

--- real-estate/src/components/Contact.jsx
+++ real-estate/src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { toast } from 'react-toastify';
+import { motion } from "framer-motion"
+
 
 const Contact = () => {
 
@@ -32,7 +34,13 @@ const [result, setResult] = React.useState("");
 
 
   return (
-    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
+    <motion.div 
+    initial={{opacity: 0, x:-200}}
+    transition={{duration: 1}}
+    whileInView={{opacity: 1, x:0}}
+    viewport={{once: true}}
+    
+    className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
         
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer <span className='underline underline-offset-4 decoration-1 under font-light'>Testimonials</span></h1>
         <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? Let's Build Yoour Future Together</p>
@@ -64,7 +72,7 @@ const [result, setResult] = React.useState("");
             <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>{result ? result : "Send Message"}</button>
         </form>
 
-    </div>
+    </motion.div>
   )
 }
 
