426d1c070d455f1ed1b5768d831eb70d86450864
Priya-azmara <priyadharsh2894@gmail.com>
2026-03-25 10:57:47 +0530

    projects component completed

--- real-estate/src/App.jsx
+++ real-estate/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
+import Projects from './components/Projects'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       
       <Header/>
       <About/>
+      <Projects/>
     </div>
   )
 }
